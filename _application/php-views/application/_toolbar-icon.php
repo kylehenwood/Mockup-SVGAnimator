55766bcc99e8dd93bfe98def7d6d7932ec7c2aa8
kyle <kyle.henwood@gmail.com>
2018-05-06 19:19:26 +1200

    icon text

---
+++ _application/php-views/application/_toolbar-icon.php
@@ -0,0 +1,8 @@
+<div class="toolbar__item">
+	<div class="toolbar-item">
+		<div class="toolbar-item__icon">
+			<div class="toolbar-icon <?php echo($iconcolor); ?> "></div>
+		</div>
+		<div class="toolbar-item__text">Text</div>
+	</div>
+</div>
--- _application/php-views/application/application-layout.php
+++ _application/php-views/application/application-layout.php
@@ -13,20 +13,31 @@
 
 <!-- toolbar -->
 <div class="layout__toolbar">
-	<ul class="toolbar-list toolbar-list--left">
-		<li class="toolbar-list__item toolbar-list__item--blue"></li>
-		<li class="toolbar-list__item toolbar-list__item--blue"></li>
-		<li class="toolbar-list__item toolbar-list__item--blue"></li>
+	<ul class="toolbar toolbar--left">
+		<?php
+			$iconcolor = 'toolbar-icon--blue';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+		?>
 	</ul>
-	<ul class="toolbar-list toolbar-list--center">
-		<li class="toolbar-list__item toolbar-list__item--pink"></li>
-		<li class="toolbar-list__item toolbar-list__item--pink"></li>
-		<li class="toolbar-list__item toolbar-list__item--pink"></li>
+	<ul class="toolbar toolbar--center">
+		<?php
+			$iconcolor = 'toolbar-icon--pink';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+		?>
 	</ul>
-	<ul class="toolbar-list toolbar-list--right">
-		<li class="toolbar-list__item toolbar-list__item--purple"></li>
-		<li class="toolbar-list__item toolbar-list__item--purple"></li>
-		<li class="toolbar-list__item toolbar-list__item--purple"></li>
+	<ul class="toolbar toolbar--right">
+		<?php
+			$iconcolor = 'toolbar-icon--purple';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+			include '_toolbar-icon.php';
+		?>
 	</ul>
 </div>
 
@@ -42,40 +53,53 @@
 
 <!-- stage -->
 <div class="layout__stage">
-	<div class="stage-ruler stage-ruler--horizontal">
-			<?php for ($x = 0; $x <= 15; $x++) { ?>
-			<div class="rule-horizontal">
-				<div class="rule-horizontal__text"><?php echo ($x*100) ?></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--1"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--2"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--3"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--4"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--5"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--6"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--7"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--8"></div>
-				<div class="rule-horizontal__hatch rule-horizontal__hatch--9"></div>
+	<div class="stage">
+		<div class="stage__ruler-corner"></div>
+		<div class="stage__ruler-highlight-vertical">
+			<div class="ruler-highlight-vertical"></div>
+		</div>
+		<div class="stage__ruler-highlight-horizontal">
+			<div class="ruler-highlight-horizontal"></div>
+		</div>
+		<div class="stage__ruler-vertical">
+			<div class="ruler ruler--vertical">
+				<?php for ($x = 0; $x <= 25; $x++) { ?>
+				<div class="rule-vertical">
+					<div class="rule-vertical__text"><?php echo ($x*100) ?></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--1"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--2"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--3"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--4"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--5"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--6"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--7"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--8"></div>
+					<div class="rule-vertical__hatch rule-vertical__hatch--9"></div>
+				</div>
+				<?php } ?>
 			</div>
-			<?php } ?>
+		</div>
+		<div class="stage__ruler-horizontal">
+			<div class="ruler ruler--horizontal">
+				<?php for ($x = 0; $x <= 25; $x++) { ?>
+				<div class="rule-horizontal">
+					<div class="rule-horizontal__text"><?php echo ($x*100) ?></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--1"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--2"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--3"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--4"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--5"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--6"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--7"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--8"></div>
+					<div class="rule-horizontal__hatch rule-horizontal__hatch--9"></div>
+				</div>
+				<?php } ?>
+			</div></div>
+		<div class="stage__artwork">
+			<div class="artwork"></div>
+		</div>
 	</div>
-	<div class="stage-ruler stage-ruler--vertical">
-			<?php for ($x = 0; $x <= 15; $x++) { ?>
-			<div class="rule-vertical">
-				<div class="rule-vertical__text"><?php echo ($x*100) ?></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--1"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--2"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--3"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--4"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--5"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--6"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--7"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--8"></div>
-				<div class="rule-vertical__hatch rule-vertical__hatch--9"></div>
-			</div>
-			<?php } ?>
-	</div>
-
-	<div class="stage"></div>
 </div>
 
 
