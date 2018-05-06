9198c944c0719b71a40c1ba38f76006f2dfd29ed
kyle <kyle.henwood@gmail.com>
2018-05-06 15:36:39 +1200

    ruler detail

--- _application/php-views/application/application-layout.php
+++ _application/php-views/application/application-layout.php
@@ -42,6 +42,39 @@
 
 <!-- stage -->
 <div class="layout__stage">
+	<div class="stage-ruler stage-ruler--horizontal">
+			<?php for ($x = 0; $x <= 15; $x++) { ?>
+			<div class="rule-horizontal">
+				<div class="rule-horizontal__text"><?php echo ($x*100) ?></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--1"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--2"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--3"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--4"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--5"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--6"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--7"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--8"></div>
+				<div class="rule-horizontal__hatch rule-horizontal__hatch--9"></div>
+			</div>
+			<?php } ?>
+	</div>
+	<div class="stage-ruler stage-ruler--vertical">
+			<?php for ($x = 0; $x <= 15; $x++) { ?>
+			<div class="rule-vertical">
+				<div class="rule-vertical__text"><?php echo ($x*100) ?></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--1"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--2"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--3"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--4"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--5"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--6"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--7"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--8"></div>
+				<div class="rule-vertical__hatch rule-vertical__hatch--9"></div>
+			</div>
+			<?php } ?>
+	</div>
+
 	<div class="stage"></div>
 </div>
 
