3866edff1a862e23f9f50f370524a8d9f4d06974
kyle <kyle.henwood@gmail.com>
2018-05-11 16:18:52 +1200

    timeline
    building timeline

--- _application/php-views/application/_toolbar-icon.php
+++ _application/php-views/application/_toolbar-icon.php
@@ -1,7 +1,9 @@
 <div class="toolbar__item">
 	<div class="toolbar-item">
 		<div class="toolbar-item__icon">
-			<div class="toolbar-icon <?php echo($iconcolor); ?> "></div>
+			<div class="toolbar-icon <?php echo($iconcolor); ?> ">
+				<div class="icon icon--16 icon--square-white"></div>
+			</div>
 		</div>
 		<div class="toolbar-item__text">Text</div>
 	</div>
--- _application/php-views/application/application-timeline.php
+++ _application/php-views/application/application-timeline.php
@@ -1,8 +1,246 @@
 <!-- timeline -->
 <div class="layout__timeline">
-	<div class="timeline-table">
-		<div class="timeline-table__layers"></div>
-		<div class="timeline-table__controls"></div>
-		<div class="timeline-table__timeline"></div>
+
+
+	<div class="timeline-layout">
+
+		<!-- title -->
+		<div class="timeline-layout__title">
+			<div class="timeline-table">
+				<div class="timeline-table__layers">
+					<div class="artboard-control">
+						<div class="artboard-control__timing"><span class="icon icon--16 icon--square-white"><span></div>
+						<div class="artboard-control__title">Timeline (Artboard Name)</div>
+						<div class="artboard-control__settings"><span class="icon icon--16 icon--square-white"><span></div>
+					</div>
+				</div>
+				<div class="timeline-table__controls">
+					<div class="controls-layer">
+						<!-- play || pause -->
+						<div class="controls-timeline__button">
+							<div class="icon icon--16 icon--square-white"></div>
+						</div>
+						<!-- loop: on || off -->
+						<div class="controls-timeline__button">
+							<div class="icon icon--16 icon--square-white"></div>
+						</div>
+					</div>
+				</div>
+				<div class="timeline-table__timeline timeline-table__timeline--relative">
+					<div class="timeline-container">
+						<?php for ($x = 0; $x <= 20; $x++) { ?>
+						<div class="timeline-rule">
+							<div class="timeline-rule__text"><?php echo($x*100); ?></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--1"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--2"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--3"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--4"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--5"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--6"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--7"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--8"></div>
+							<div class="timeline-rule__hatch timeline-rule__hatch--9"></div>
+						</div>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- content -->
+		<div class="timeline-layout__content">
+			<div class="timeline-table">
+
+				<!-- layers column -->
+				<div class="timeline-table__layers">
+					<div class="layer-svg">&lt;svg id="raygun-artboard"&gt;</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-1">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">Artwork</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-2">
+								<div class="layer-item">
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+									<div class="layer-item__title">charging-circle</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-2">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">beam</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+									<div class="layer-item__title">beam-blue</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+									<div class="layer-item__title">beam-white</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-2">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">raygun</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+									<div class="layer-item__title">gun-tip</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+									<div class="layer-item__title">fin-background</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">gun-tip</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">gun-background</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+							<div class="layer__item layer__item--indent-3">
+								<div class="layer-item">
+									<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+									<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+									<div class="layer-item__title">mask-shine</div>
+									<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+								</div>
+							</div>
+							<div class="layer__styles"></div>
+						</div>
+					<div class="layer">
+						<div class="layer__item layer__item--indent-4">
+							<div class="layer-item">
+								<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+								<div class="layer-item__icon"><span class="icon icon--16 icon--square-pink"><span></div>
+								<div class="layer-item__title">clip-path</div>
+								<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+							</div>
+						</div>
+						<div class="layer__styles"></div>
+					</div>
+					<div class="layer">
+						<div class="layer__item layer__item--indent-5">
+							<div class="layer-item">
+								<div class="layer-item__icon"><span class="icon icon--16 icon--square-blue"><span></div>
+								<div class="layer-item__title">sheen</div>
+								<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+							</div>
+						</div>
+						<div class="layer__styles"></div>
+					</div>
+					<div class="layer">
+						<div class="layer__item layer__item--indent-3">
+							<div class="layer-item">
+								<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+								<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+								<div class="layer-item__title">gun-metal</div>
+								<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+							</div>
+						</div>
+						<div class="layer__styles"></div>
+					</div>
+					<div class="layer">
+						<div class="layer__item layer__item--indent-3">
+							<div class="layer-item">
+								<div class="layer-item__expand"><span class="icon icon--16 icon--square-white"><span></div>
+								<div class="layer-item__icon"><span class="icon icon--16 icon--square-green"><span></div>
+								<div class="layer-item__title">gun-detail</div>
+								<div class="layer-item__style"><span class="icon icon--16 icon--square-white"><span></div>
+							</div>
+						</div>
+						<div class="layer__styles"></div>
+					</div>
+				</div>
+
+				<!-- layer controls column -->
+				<div class="timeline-table__controls">
+					<div class="controls-svg"></div>
+					<?php for ($x = 0; $x <= 14; $x++) { ?>
+					<div class="controls-layer">
+						<!-- hide -->
+						<div class="controls-timeline__button">
+							<div class="icon icon--16 icon--square-white"></div>
+						</div>
+						<!-- lock -->
+						<div class="controls-timeline__button">
+							<div class="icon icon--16 icon--square-white"></div>
+						</div>
+					</div>
+					<?php } ?>
+				</div>
+
+				<!-- layer timeline column -->
+				<div class="timeline-table__timeline">
+					<div class="timeline-svg"></div>
+					<?php for ($x = 0; $x <= 14; $x++) { ?>
+					<div class="timeline-layer"></div>
+					<?php } ?>
+				</div>
+			</div>
+		</div>
+	</div>
 	</div>
 </div>
